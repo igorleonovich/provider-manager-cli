bc004c99c5ec71bd7f805ca5cc312d0b17747b4e
name <email>
2020-02-22 12:20:07 +0300

    no message

--- Sources/ProviderManager/main.swift
+++ Sources/ProviderManager/main.swift
@@ -1,7 +1,9 @@
 import Foundation
 
 var registry = CommandRegistry(usage: "<command> <options>", overview: "Provider Manager")
+registry.register(command: StatusCommand.self)
 registry.register(command: DeployCommand.self)
+registry.register(command: RemoveCommand.self)
 registry.run()
 
 
